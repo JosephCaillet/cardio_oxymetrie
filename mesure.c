9648181e700cc961c625f57cdfb6c04a6cf0b47a
Nephylhim <thomas.coussot@laposte.net>
2016-03-31 11:50:31 +0200

    comment mesure

--- mesure.c
+++ mesure.c
@@ -1,12 +1,29 @@
+/*==============================================================================================================*/
+/*	Nom : 		mesure.c																						*/
+/*	Auteur : 	Joseph CAILLET																					*/
+/*	Update :	Thomas COUSSOT																					*/
+/*	Desc : 		La fonction a pour but de calculer le BPM et le spO2 avec les données absorp qui arrivent petit */
+/*				à petit.																						*/
+/*==============================================================================================================*/
+
 #include "mesure.h"
 #include <stdio.h>
 
+/*------------------------------------------------------------------------------*/
+/*	Fonction : 	mesure															*/
+/*	Params :	absorp absorb, Acmesures* acR et acIR							*/
+/*	Retour :	oxy* oxyDatas, Acmesures* acR et acIR							*/
+/*	Desc :		La fonction reçoit les dernières valeurs d'absorption ainsi que */
+/*				des sauvegardes de données sur acR et acIR. La fonction renvoie */
+/*				les dernières valeurs de BPM et de spO2 calculées et met à jour */
+/*				les données sauvegardées.										*/
+/*------------------------------------------------------------------------------*/
 void mesure(oxy* oxy, absorp absorp, AcMesures* acRm, AcMesures* acIRm)
 {
 	int periodeAquise = 0;
 	//static int mesure = 0;
 
-	//majMaxMinSeuil
+	//-- On détecte le dépassement de seuil et on met à jour le max et le min si nécessaire --//
 	majMaxMinDepasseSeuil(acRm, absorp.acr);
 	majMaxMinDepasseSeuil(acIRm, absorp.acir);
 
@@ -14,11 +31,11 @@ void mesure(oxy* oxy, absorp absorp, AcMesures* acRm, AcMesures* acIRm)
 //	majPassageZero(acRm, absorp.acr);
 //	majPassageZero(acIRm, absorp.acir);
 
-	//updBpm
+	//-- On détecte si une nouvelle période a été acquise sur une des deux courbes (r et ir)(ou les deux) --//
 	periodeAquise += majBpm(acRm, absorp.acr);
 	periodeAquise += majBpm(acIRm, absorp.acir);
 
-	//updSPo2	
+	//-- Si au moins une nouvelle période a été détecté, on met à jour le spO2 et le BPM --//
 	if(periodeAquise > 0)
 	{
 //		puts("Avant spo2 :");
@@ -34,6 +51,16 @@ void mesure(oxy* oxy, absorp absorp, AcMesures* acRm, AcMesures* acIRm)
 //85spo2
 //70bpm
 
+/*------------------------------------------------------------------------------*/
+/*	Fonction : 	majMaxMinDepasseSeuil											*/
+/*	Params :	AcMesures* ac, int acNew										*/
+/*	Retour :	Acmesures* ac													*/
+/*	Desc :		La fonction reçoit la dernière valeur d'absorption ainsi que	*/
+/*				des sauvegardes de données sur ac. Elle met à jour le min et le */
+/*				max si nécessaire. Si tel est le cas, il y a également un		*/
+/*				dépassement de seuil (le max n'est jamais inférieur au seuil 	*/
+/*				haut et inversement pour le min).								*/
+/*------------------------------------------------------------------------------*/
 void majMaxMinDepasseSeuil(AcMesures* ac, int acNew)
 {
 	if(acNew > ac->max)
@@ -48,6 +75,14 @@ void majMaxMinDepasseSeuil(AcMesures* ac, int acNew)
 	}
 }
 
+/*------------------------------------------------------------------------------*/
+/*	Fonction : 	majBpm															*/
+/*	Params :	AcMesures* ac, float ech_ac										*/
+/*	Retour :	int nouvellePeriode, AcMesures* ac								*/
+/*	Desc :		La fonction calcul le nouveau bpm avec la nouvelle valeur de	*/
+/*				compteur ainsi que l'ancienne. Il y a également réinitialisation*/
+/*				de données sauvegardées sur ac.									*/
+/*------------------------------------------------------------------------------*/
 int majBpm(AcMesures* ac, float ech_ac)
 {
 	static float lastbpm = 425;
@@ -56,6 +91,7 @@ int majBpm(AcMesures* ac, float ech_ac)
 //		printf("Une période à été aquise sur : %s\n", ac->s);
 		//affAcMesure(*ac);
 		
+		//-- Réinitialisation des seuils passés ainsi que les max et min --//
 		ac->seuilHautPasse = 0;
 		ac->seuilBasPasse = 0;
 		//ac->passagePar0 = 1;
@@ -65,10 +101,11 @@ int majBpm(AcMesures* ac, float ech_ac)
 		ac->minValid = ac->min;
 		ac->min = SEUIL_BAS;
 
+		//-- maj du bpm --//
 		ac->bpm = (float)30000 / (((float)ac->nbPoints + (float)lastbpm)/2)  ;
 //		ac->bpm = (float)30000 / (float)ac->nbPoints;
 		printf("cpt : %d\tcpt_old : %1f\tbpm : %f\tbpm_old : %f\tbpm_moy : %f\n", ac->nbPoints, lastbpm, (float)30000/(float)ac->nbPoints, (float)30000/(float)lastbpm, ac->bpm);
-		lastbpm = ac->nbPoints;
+		lastbpm = ac->nbPoints; //on met à jour l'ancienne valeur (de compteur, le fonctionnement a été modifié entre temps, d'où le nom peu représentatif de la variable)
 		ac->nbPoints = 1;
 		
 		return 1;
@@ -79,15 +116,23 @@ int majBpm(AcMesures* ac, float ech_ac)
 	return 0;
 }
 
+/*------------------------------------------------------------------------------*/
+/*	Fonction : 	calculSPo2														*/
+/*	Params :	float acRmax, float acRmin, float acIRmax, float acIRmin,		*/
+/*				float dcR, float dcIR											*/
+/*	Retour :	float spO2														*/
+/*	Desc :		La fonction calcul le nouveau spO2 avec les valeurs de max et	*/
+/*				min d'acr et d'acir ainsi que dcr et dcir.						*/
+/*------------------------------------------------------------------------------*/
 float calculSPo2(float acRmax, float acRmin, float acIRmax, float acIRmin, float dcR, float dcIR)
 {
 	//puts("Ds spo2 :");
 	//printf("acRmax : %f, acRmin : %f\nacIRmax : %f, acIRmin : %f\ndcR : %f, dcIR : %f\n", acRmax, acRmin, acIRmax, acIRmin, dcR, dcIR);
-	float ptpACr = acRmax - acRmin;
+	float ptpACr = acRmax - acRmin; //Valeur peek-to-peek d'acr
 	float rapport1 = ptpACr / dcR;
 //	printf("le rapport1 vaut : %f\n", rapport1);
 
-	float ptpACir = acIRmax - acIRmin;
+	float ptpACir = acIRmax - acIRmin; //Valeur peek-to-peek d'acir
 	float rapport2 = ptpACir / dcIR;
 //	printf("le rapport2 vaut : %f\n", rapport2);
 
@@ -95,6 +140,14 @@ float calculSPo2(float acRmax, float acRmin, float acIRmax, float acIRmin, float
 	return  convertRatioToSPO2( rapport1 / rapport2 );
 }
 
+/*------------------------------------------------------------------------------*/
+/*	Fonction : 	convertRatioToSPO2												*/
+/*	Params :	float ratio														*/
+/*	Retour :	float spO2														*/
+/*	Desc :		La fonction calcul le  spO2 en fonction du ratio envoyé. La		*/
+/*				courbe de spO2/ratio peut-être représentée par deux droites dont*/
+/*				les coeficients sont rentrés en dur dans la fonction			*/
+/*------------------------------------------------------------------------------*/
 float convertRatioToSPO2(float ratio)
 {
 	if(ratio < 1)
@@ -104,6 +157,7 @@ float convertRatioToSPO2(float ratio)
 	return -35.71429 * ratio + 121.4285714;
 }
 
+//-- Fonction de test afin d'afficher des résultats intermédiaires --//
 void affAcMesure(AcMesures ac)
 {
 	printf("-- %s --\n", ac.s);
@@ -117,6 +171,13 @@ void affAcMesure(AcMesures ac)
 	printf("Max : %f\n", ac.max);
 }
 
+/*------------------------------------------------------------------------------*/
+/*	Fonction : 	mesureTest														*/
+/*	Params :	char* nomFic													*/
+/*	Retour :	bpm et spO2														*/
+/*	Desc :		La fonction permet de tester la fonction mesure de manière		*/
+/*				unitaire.														*/
+/*------------------------------------------------------------------------------*/
 oxy mesureTest(char* str){
 	oxy oxyDatas;
 	absorp absorb;
